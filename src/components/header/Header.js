a67b27ecd2a9e6a4bd713f8b086bac840a44a162
Ta Duc Binh <binhtd@aratech.vn>
2022-09-12 10:54:25 +0700

    fix logic header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,69 +1,71 @@
 import React, { useEffect, useRef } from "react";
 import "./header.scss";
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 import logo from "../../assets/tmovie.png";
 
 const headerNav = [
-  {
-    display: "Trang chủ",
-    path: "/",
-  },
-  {
-    display: "Phim chiếu rạp",
-    path: "/movie",
-  },
-  {
-    display: "Phim truyền hình",
-    path: "/tv",
-  },
+    {
+        display: "Trang chủ",
+        path: "/",
+    },
+    {
+        display: "Phim chiếu rạp",
+        path: "/movie",
+    },
+    {
+        display: "Phim truyền hình",
+        path: "/tv",
+    },
 ];
 
 const Header = () => {
-  const { pathName } = useLocation();
-  const headerRef = useRef(null);
+    const { pathName } = useLocation();
+    const headerRef = useRef(null);
 
-  const active = headerNav.findIndex((e) => e.path === pathName);
+    const active = headerNav.findIndex((e) => e.path === pathName);
 
-  useEffect(() => {
-    const shrinkHeader = () => {  
-      if (
-        document.body.scrollTop > 100 ||
-        document.documentElement.scrollTop > 100
-      ) {
-        headerRef.current.classList.add("shrink");
-      } else {
-        headerRef.current.classList.remove("shrink");
-      }
+    useEffect(() => {
+        const shrinkHeader = () => {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                headerRef.current.classList.add("shrink");
+            } else {
+                headerRef.current.classList.remove("shrink");
+            }
+        };
+        window.addEventListener("scroll", shrinkHeader);
+
+        return () => {
+            window.removeEventListener("scroll", shrinkHeader);
+        };
+    }, []);
+
+    const goTop = () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     };
 
-    window.addEventListener('scroll',shrinkHeader)
-
-    return () => {
-    window.removeEventListener('scroll',shrinkHeader)
-
-    };
-  }, []);
-
-  return (
-    <div ref={headerRef} className="header">
-      <div className="header__wrap container">
-        <div className="logo">
-          <img src={logo} alt="" />
-          <Link to="/">tMovie</Link>
+    return (
+        <div ref={headerRef} className="header">
+            <div className="header__wrap container">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <NavLink exact to="/">
+                        tMovie
+                    </NavLink>
+                </div>
+                <ul className="header__nav">
+                    {headerNav.map((e, i) => (
+                        <li key={i}>
+                            <NavLink exact to={e.path} className={`${i === active ? "active" : ""}`} onClick={goTop}>
+                                {e.display}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-        <ul className="header__nav">
-          {headerNav.map((e, i) => (
-            <li key={i} className={`${i === active ? "active" : ""}`}>
-              <Link to={e.path}>{e.display}</Link>
-            </li>
-            
-          ))}
-
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
